8165b94cd5607fa81a527df473013693c05b38f0
Schlabach <matthewshawn89@gmail.com>
2023-01-18 06:37:02 -0500

    initial commit

--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using PalindromeExercise;
 using System;
 using Xunit;
 
@@ -5,10 +6,19 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("spaceship", false)]
+        [InlineData("Racecar", true)]
 
+
+        public void PalidromeTest(string word, bool expected)
+        {
+            var test = new WordSmith();
+
+            var actual = test.IsAPalindrome(word);
+
+            Assert.Equal(expected, actual); 
         }
     }
 }
